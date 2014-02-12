f93366fc4739f2a92babf01af4e5dad4453e3803
Chuck Boecking <chuck@chuboe.com>
2014-02-12 00:14:46 +0000

    chuboe_restore_s3cmd.sh created online with Bitbucket

---
+++ utils/chuboe_restore_s3cmd.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Created Version 1 and 2 by Sandy Corsillo
+LOGFILE="/var/log/ex_restore.log"
+ADEMROOTDIR="/opt/idempiere-server"
+
+echo LOGFILE="$LOGFILE" >> "$LOGFILE"
+echo ADEMROOTDIR="$ADEMROOTDIR" >> "$LOGFILE"
+
+cd "$ADEMROOTDIR"/utils
+echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+echo ademres: -------          STARTING iDempiere Daily Restore           ------- >> "$LOGFILE"
+echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+if sudo service idempiere stop >> "$LOGFILE"
+then
+    echo ademres: Idempiere Stopped >> "$LOGFILE"
+    if s3cmd sync --delete s3://iDempiere_backup2/latest/ "$ADEMROOTDIR"/data/ex_restore_backups/ >> "$LOGFILE"
+    then
+        cd "$ADEMROOTDIR"/data
+        rm ExpDat.dmp
+        jar xf "$ADEMROOTDIR"/data/ex_restore_backups/*.jar
+        cd "$ADEMROOTDIR"/utils
+        if ./RUN_DBRestore.sh #>> "$LOGFILE"
+        then
+            if sudo service idempiere start >> "$LOGFILE"
+            then
+                echo ademres: Idempiere Started Back Up >> "$LOGFILE"
+            else
+                echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+                echo ademres: -------         Remote iDempiere Backup FAILED!             ------- >> "$LOGFILE"
+                echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+                sudo cp /var/log/ex_restore.log /var/log/ex_restore_logs/ex_restore_"$(date +'%d_%m_%Y')".log
+                sudo cp /dev/null /var/log/ex_restore.log
+                exit 1
+            fi
+        else
+            echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+            echo ademres: -------          Idempiere Restore FAILED!                  ------- >> "$LOGFILE"
+            echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+            sudo cp /var/log/ex_restore.log /var/log/ex_restore_logs/ex_restore_"$(date +'%d_%m_%Y')".log
+            sudo cp /dev/null /var/log/ex_restore.log
+            exit 1
+        fi
+    else
+        echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+        echo ademres: -------         iDempiere Sync From Remote S3 FAILED!       ------- >> "$LOGFILE"
+        echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+        sudo cp /var/log/ex_restore.log /var/log/ex_restore_logs/ex_restore_"$(date +'%d_%m_%Y')".log
+        sudo cp /dev/null /var/log/ex_restore.log
+        exit 1
+    fi
+else
+    echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+    echo ademres: -------         iDempiere Stop Service FAILED!              ------- >> "$LOGFILE"
+    echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+    sudo cp /var/log/ex_restore.log /var/log/ex_restore_logs/ex_restore_"$(date +'%d_%m_%Y')".log
+    sudo cp /dev/null /var/log/ex_restore.log
+    exit 1 
+fi
+echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+echo ademres: -------         COMPLETED iDempiere Daily Restore           ------- >> "$LOGFILE"
+echo ademres: ------------------------------------------------------------------- >> "$LOGFILE"
+sudo cp /var/log/ex_restore.log /var/log/ex_restore_logs/ex_restore_"$(date +'%d_%m_%Y')".log
+sudo cp /dev/null /var/log/ex_restore.log
+exit 0
