eb05685791f7dde79a2d188156898ee937470b47
Chuck Boecking <chuck@chuboe.com>
2014-05-07 03:22:09 +0000

    idempiere_install_script_master_linux.sh edited online with Bitbucket

--- idempiere_install_script_master_linux.sh
+++ idempiere_install_script_master_linux.sh
@@ -377,7 +377,6 @@ then
 	echo "----> Bleeding Edge: http://www.globalqss.com/wiki/index.php/IDempiere/Downloading_Hot_Installers">>/home/$OSUSER/$README
 	echo "----> Current Stable Release: http://sourceforge.net/projects/idempiere/files/v2.0/swing-client/">>/home/$OSUSER/$README
 	echo "Issue the following commands to enable s3cmd and create an iDempiere backup bucket in S3.">>/home/$OSUSER/$README
-	# add the s3cmd command as the last step to the existing backup script
         echo "--> s3cmd --configure">>/home/$OSUSER/$README
 	echo "----> get your access key and secred key by logging into your AWS account">>/home/$OSUSER/$README
 	echo "----> enter a password. Chose something different than your AWS password. Write it down!!">>/home/$OSUSER/$README
