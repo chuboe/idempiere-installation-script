7557f1cec29ac0c726dafb23243dcb93c0a94487
Chuck Boecking <chuck@chuboe.com>
2014-05-22 22:46:40 +0000

    chuboe_obfuscation.sql edited online with Bitbucket

--- utils/chuboe_obfuscation.sql
+++ utils/chuboe_obfuscation.sql
@@ -5,28 +5,33 @@
 
 -- Business Partner 
 update C_BPartner bp
-set (name, description, name2, alias, acronym, taxId, url, emailConfirm, XName)
-= ( select 'bp' || bp2.c_bpartner_id, 'bp' || bp2.c_bpartner_id, null, null, null, null, null, null, null
-    from c_bpartner bp2
-    where bp2.c_bpartner_id = bp.c_bpartner_id)
+set 
+name = 'bp' || bp2.c_bpartner_id, 
+description = null, 
+name2 = null, 
+taxId = null, 
+url = null
+from c_bpartner bp2
 ;
 
 -- Business Partner Location
 update C_BPartner_Location bpl
-set (name, Phone, LocCompanyName, Alias, Acronym, Phone2, Fax, BPAcronym, BPAlias, Description, PreAlertEmails, FedTaxID, EmailConfirm, EmailInvoice)
-= ( select 'bplocation' || bpl2.c_bpartner_location_id, '512 555-5555', null, null, null, null, null, null, null, null, null, null, null, null
-    from c_bpartner_location bpl2
-    where bpl2.c_bpartner_location_id = bpl.c_bpartner_location_id)
+set 
+name='bplocation' || bpl2.c_bpartner_location_id, 
+Phone='555-555-5555', 
+Phone2=null, 
+Fax=null
+from c_bpartner_location bpl2
 ;
 
 -- User
 update AD_User u
-set (Name, Description)
-= ( select 'user' || u.ad_user_id, 'user' || u.ad_user_id
-    from ad_user u2
-    where u2.ad_user_id = u.ad_user_id)
-where name <> 'SuperUser'
-  and ((c_bpartner_id is null) 
+set 
+Name='user' || u.ad_user_id, 
+Description='user' || u.ad_user_id
+from ad_user u2
+where u.name <> 'SuperUser'
+  and ((u.c_bpartner_id is null) 
     or (
       SELECT xbp.ISEMPLOYEE 
       FROM C_BPARTNER xbp 
@@ -34,18 +39,31 @@ where name <> 'SuperUser'
 ;
 
 update AD_User u
-set (Password, Email, Phone, Email2, Phone2, Fax, Mobile, EmailConfirm, SIFBCCEmail, EmailUserPW, EmailUser, EmailVerify, BackUpPassword, Birthday)
-= ( select 'password', 'test@idempiere.com', '512 555-5555', null, null, null, null, null, null, null, null, null, null, null
-    from ad_user u2
-    where u2.ad_user_id = u.ad_user_id)
+set 
+Password='password', 
+Email='test@idempiere.com',  
+Phone='512 555-5555', 
+Phone2=null, 
+Fax=null, 
+EmailUserPW=null, 
+EmailUser=null, 
+EmailVerify=null, 
+Birthday=null
+from ad_user u2
 ;
 
 -- Location
 update C_Location loc
-set (Address1, Address2, Address3, Address4)
-= ( select '1313 Mockingbird Ln', null, null, null
-    from c_location loc2
-    where loc2.c_location_id = loc.c_location_id)
+set 
+Address1='1313 Mockingbird Ln', 
+Address2=null, 
+Address3=null, 
+Address4=null,
+city=null,
+postal=null,
+c_region_id=null,
+c_country_id=120
+from c_location loc2
 ;
 
 -- Bank Account
