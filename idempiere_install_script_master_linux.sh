1986681c302287cc34c6ee321ed8f6caa0d2ba2f
Chuck Boecking <chuck@chuboe.com>
2014-06-13 12:47:04 +0000

    User documentation for changing time zone

--- idempiere_install_script_master_linux.sh
+++ idempiere_install_script_master_linux.sh
@@ -406,6 +406,8 @@ then
 	echo "To install swing clients for other OS's, go to:">>/home/$OSUSER/$README
 	echo "----> Bleeding Edge: http://www.globalqss.com/wiki/index.php/IDempiere/Downloading_Hot_Installers">>/home/$OSUSER/$README
 	echo "----> Current Stable Release: http://sourceforge.net/projects/idempiere/files/v2.0/swing-client/">>/home/$OSUSER/$README
+	echo "">>/home/$OSUSER/$README
+	echo "">>/home/$OSUSER/$README
 	echo "Issue the following commands to enable s3cmd and create an iDempiere backup bucket in S3.">>/home/$OSUSER/$README
 	echo "----> s3cmd --configure">>/home/$OSUSER/$README
 	echo "--------> get your access key and secred key by logging into your AWS account">>/home/$OSUSER/$README
@@ -413,6 +415,10 @@ then
 	echo "--------> Accept the default path to GPG">>/home/$OSUSER/$README
 	echo "--------> Answer yes to HTTPS">>/home/$OSUSER/$README
 	echo "----> s3cmd mb s3://iDempiere_backup">>/home/$OSUSER/$README
+	echo "">>/home/$OSUSER/$README
+	echo "">>/home/$OSUSER/$README
+	echo "To update your server's timezone, run this command:">>/home/$OSUSER/$README
+	echo "----> sudo dpkg-reconfigure tzdata">>/home/$OSUSER/$README
 
 #not indented because of file input
 sh console-setup.sh <<!
