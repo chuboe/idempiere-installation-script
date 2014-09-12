59d6edee9a461ebd4343e55d03322a951639b0e8
Chuck Boecking <chuck@chuboe.com>
2014-09-12 17:19:30 +0000

    01_Source.sql edited online with Bitbucket

--- apps/QuickSearch/01_Source.sql
+++ apps/QuickSearch/01_Source.sql
@@ -2,92 +2,92 @@
 create or replace view chuboe_quicksearch as
 Select value, m_product_id as record_id, 208::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Product'::text as Description
+'Product - Value'::text as Description
 from M_Product
 union all
 Select Name, m_product_id as record_id, 208::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Product'::text as Description
+'Product - Name'::text as Description
 from M_Product
 union all
 Select UPC, m_product_id as record_id, 208::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Product'::text as Description
+'Product - UPC'::text as Description
 from M_Product
 union all
 Select sku, m_product_id as record_id, 208::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Product'::text as Description
+'Product - SKU'::text as Description
 from M_Product
 union all
 Select value, c_bpartner_id as record_id, 291::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Business Partner'::text as Description
+'Business Partner - Value'::text as Description
 from c_bpartner
 union all
 Select name, c_bpartner_id as record_id, 291::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Business Partner'::text as Description
+'Business Partner - Name'::text as Description
 from c_bpartner
 union all
 Select name2, c_bpartner_id as record_id, 291::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Business Partner'::text as Description
+'Business Partner - Name2'::text as Description
 from c_bpartner
 union all
 Select ReferenceNo, c_bpartner_id as record_id, 291::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Business Partner'::text as Description
+'Business Partner - ReferenceNo'::text as Description
 from c_bpartner
 union all
 Select name, ad_user_id as record_id, 114::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Contact or User'::text as Description
+'Contact or User - Name'::text as Description
 from ad_user
 union all
 Select email, ad_user_id as record_id, 114::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Contact or User'::text as Description
+'Contact or User - email'::text as Description
 from ad_user
 union all
 Select name, c_bpartner_location_id as record_id, 293::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'BP Location'::text as Description
+'BP Location - Name'::text as Description
 from c_bpartner_location
 union all
 Select documentno, c_order_id as record_id, 259::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Order'::text as Description
+'Order - DocNo'::text as Description
 from c_order
 union all
 Select documentno, c_invoice_id as record_id, 318::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Invoice'::text as Description
+'Invoice - DocNo'::text as Description
 from c_invoice
 union all
 Select documentno, c_payment_id as record_id, 335::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Payment'::text as Description
+'Payment - DocNo'::text as Description
 from c_payment
 union all
 Select documentno, m_inout_id as record_id, 319::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Ship/Receipt'::text as Description
+'Ship/Receipt - DocNo'::text as Description
 from m_inout
 union all
 Select documentno, m_inventory_id as record_id, 321::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Physical Inventory'::text as Description
+'Physical Inventory - DocNo'::text as Description
 from m_inventory
 union all
 Select documentno, m_movement_id as record_id, 323::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Inventory Move'::text as Description
+'Inventory Move - DocNo'::text as Description
 from m_movement
 union all
 Select documentno, m_production_id as record_id, 325::numeric as ad_table_id,
 ad_client_id, 0::numeric as ad_org_id, created, createdby, updated, updatedby,
-'Production'::text as Description
+'Production - DocNo'::text as Description
 from m_production
 ;
 
