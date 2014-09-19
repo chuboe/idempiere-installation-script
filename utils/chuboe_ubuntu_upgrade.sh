6e49419bbb0503681221ec16b0e415a2f865106f
Chuck Boecking <chuck@chuboe.com>
2014-09-19 15:43:01 +0000

    chuboe_ubuntu_upgrade.sh edited online with Bitbucket

--- utils/chuboe_ubuntu_upgrade.sh
+++ utils/chuboe_ubuntu_upgrade.sh
@@ -9,6 +9,7 @@
 #  http://debian-handbook.info/browse/wheezy/sect.apt-get.html
 #  http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
 #  http://stackoverflow.com/questions/7039520/bash-how-to-detect-whether-apt-get-requires-a-reboot
+#  http://askubuntu.com/questions/110477/how-do-i-upgrade-to-a-newer-version-of-ubuntu  (lots of info here in replies!!!)
 
 # To use this file, do one of the following tasks:
 #  1. run this periodically
