7c126030af2b3868002efb017d93f8c5903c1454
Chuck Boecking <chuck@chuboe.com>
2014-09-19 15:30:53 +0000

    added lots of notes.

--- utils/chuboe_ubuntu_upgrade.sh
+++ utils/chuboe_ubuntu_upgrade.sh
@@ -8,6 +8,14 @@
 # References:
 #  http://debian-handbook.info/browse/wheezy/sect.apt-get.html
 #  http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+#  http://stackoverflow.com/questions/7039520/bash-how-to-detect-whether-apt-get-requires-a-reboot
+
+# To use this file, do one of the following tasks:
+#  1. run this periodically
+#  2. create a cron job to run this every weekend. 
+#		Make sure you have a process to monitoring when the server needs to be rebooted. 
+#		It will not reboot by itself. The statement: [ -f /var/run/reboot-required ] will tell you if you need a reboot.
+# 		Just in case you are monitoring such things.
 
 unset UCF_FORCE_CONFFOLD
 export UCF_FORCE_CONFFNEW=YES
