56921d723b5fa439227ccdb0a327723ca6a66e1e
Chuck Boecking <chuck@chuboe.com>
2014-09-19 15:24:47 +0000

    added references

--- utils/chuboe_ubuntu_upgrade.sh
+++ utils/chuboe_ubuntu_upgrade.sh
@@ -5,6 +5,10 @@
 # Since the script installs postgresql through the apt-get process, the apt-get upgrade should also install security patches as well.
 #  - I have not absolutely confirmed the above statement.
 
+# References:
+#  http://debian-handbook.info/browse/wheezy/sect.apt-get.html
+#  http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+
 unset UCF_FORCE_CONFFOLD
 export UCF_FORCE_CONFFNEW=YES
 ucf --purge /boot/grub/menu.lst
