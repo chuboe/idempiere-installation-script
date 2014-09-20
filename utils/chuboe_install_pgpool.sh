db196ca6989f79166ed5215746865ca11b203fb3
Chuck Boecking <chuck@chuboe.com>
2014-09-20 15:28:53 +0000

    chuboe_install_pgpool.sh created online with Bitbucket

---
+++ utils/chuboe_install_pgpool.sh
@@ -0,0 +1,13 @@
+# Note: this is a working document - it is not a script yet.
+
+# Set the following
+# start here: https://github.com/bmomjian/pgpool2/blob/master/pgpool.conf.sample-stream
+# backend_flag0 = 'DISALLOW_TO_FAILOVER'
+# backend_flag1 = 'DISALLOW_TO_FAILOVER'
+# num_init_children = # depends on math
+# max_pool = # depends on math
+# replication_mode = off
+# load_balance_mode = on
+# master_slave_mode = on
+# master_slave_sub_mode = 'stream'
+# parallel_mode = off
