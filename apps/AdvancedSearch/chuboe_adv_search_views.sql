016e96607e00a842b7acc9a053b8ff2a903aa761
Chuck Boecking <chuck@chuboe.com>
2014-09-22 19:03:34 +0000

    chuboe_adv_search_views.sql edited online with Bitbucket

--- apps/AdvancedSearch/chuboe_adv_search_views.sql
+++ apps/AdvancedSearch/chuboe_adv_search_views.sql
@@ -6,8 +6,19 @@ o.created, o.createdby, o.updated, o.updatedby,
 o.documentno, o.c_order_id, ol.c_orderline_id,
 o.c_bpartner_id, o.c_bpartner_location_id,
 o.bill_bpartner_id, o.bill_location_id,
-ol.m_product_id, ol.c_charge_id, o.datepromised
+ol.m_product_id, ol.c_charge_id, o.datepromised,
+ol.line
 from c_order o
 join c_orderline ol on o.c_order_id = ol.c_order_id
 ;
 
+Create or replace view chuboe_adv_search_inout as
+select o.ad_client_id, o.ad_org_id,
+o.created, o.createdby, o.updated, o.updatedby,
+o.documentno, o.m_inout_id, ol.m_inoutline_id,
+o.c_bpartner_id, o.c_bpartner_location_id,
+ol.m_product_id, ol.c_charge_id, o.movementdate,
+ol.line
+from m_inout o
+join m_inoutline ol on o.m_inout_id= ol.m_inout_id
+;
