7e2c973cabc3080ceac723776cd8b362ffa09265
Chuck Boecking <chuck@chuboe.com>
2014-10-09 20:57:16 +0000

    Updated the way mate desktop gets installed

--- idempiere_install_script_master_linux.sh
+++ idempiere_install_script_master_linux.sh
@@ -404,16 +404,13 @@ then
 	echo "Installing desktop components because IS_INSTALL_DESKTOP == Y">>/home/$OSUSER/$README
 
 	#nice MATE desktop installation (compatible with 14.04)
-	sudo apt-get install xrdp -y
-	sudo add-apt-repository "deb http://repo.mate-desktop.org/archive/1.8/ubuntu $(lsb_release -cs) main"
-	wget -q http://mirror1.mate-desktop.org/debian/mate-archive-keyring.gpg -O- | sudo apt-key add -
+	sudo apt-get install -y xrdp
+	sudo apt-add-repository -y ppa:ubuntu-mate-dev/ppa
+	sudo apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
 	sudo apt-get update
-	sudo apt-get install -y --force-yes mate-core mate-desktop-environment mate-notification-daemon
+	sudo apt-get install -y mate-desktop-environment
 	echo mate-session> ~/.xsession
-
-	#gives you the ability to reconnect to a port - usually 5910
 	sudo sed -i "s|port=-1|port=ask-1|" /etc/xrdp/xrdp.ini
-
 	sudo apt-get install -y chromium-browser gimp xarchiver
 	sudo service xrdp restart
 
