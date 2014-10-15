4bce2381f02cb1b715450f22ed3dcea553e6fe77
Chuck Boecking <chuck@chuboe.com>
2014-10-15 03:14:41 +0000

    chuboe_favorite_misc_change.sql created online with Bitbucket

---
+++ favorites/chuboe_favorite_misc_change.sql
@@ -0,0 +1,34 @@
+--set GL Journal Description to context date by default.
+update ad_column 
+set defaultvalue = '@SQL=SELECT ''@#Date@ - ReasonForGLJournal'' FROM DUAL' 
+WHERE AD_Column_ID=1630
+;
+
+--remove existing isDefaultFocus values from the below windows/tabs
+update ad_field
+set isDefaultFocus = 'N'
+where ad_tab_id in (187, 293, 200008) --SOLine, POLine, GL Journal Line)
+;
+
+--set isDefaultFocus values for the below windows/tabs
+update ad_field
+set isDefaultFocus = 'Y'
+where (lower(name) = 'product'
+and ad_tab_id in (187, 293)) --SOLine, POLine
+or (lower(name) = 'account'
+and ad_tab_id in (200008)) --GL Journal Line
+;
+
+--Set the default value for the Material Receipt window => Generate Invoice From MR process => Pricelist parameter.
+--otherwise, the process chooses the wrong pricelist.
+update AD_Process_Para
+set defaultvalue = -1
+where AD_Process_Para_ID=183
+;
+
+--set the default to no. If yes, the system goes into a bad state where random addresses are choses for things like check runs.
+update ad_column
+set defaultvalue = 'N'
+where ad_table_id = 293 --c_bpartner_location
+and lower(columnname) in ('isbillto', 'ispayfrom', 'isremitto')
+;
