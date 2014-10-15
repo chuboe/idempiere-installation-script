9d690bd8d9d8ef4b5d838285ed6b85982b5f3335
Chuck Boecking <chuck@chuboe.com>
2014-10-15 02:29:39 +0000

    chuboe_favorite_ad_org_default_change.sql created online with Bitbucket

---
+++ favorites/chuboe_favorite_ad_org_default_change.sql
@@ -0,0 +1,27 @@
+create table chuboe_favorite_ad_org_default_change as
+select lower(tablename) as tablename from ad_table where lower(tablename) in ('m_product', 'c_bpartner', 'c_paymentterm', 'c_bank', 'c_bp_group', 'c_bp_relation', 'c_bpartner_location', 'ad_user', 'c_calendar', 'c_charge', 'c_uom', 'm_attribute', 'm_attributeset', 'm_pricelist', 'm_pricelist_version', 'm_product_category', 'r_request', 'r_requesttype', 'r_resolution', 'r_status', 's_resource', 'r_statuscategory')
+;
+
+create table chuboe_favorite_ad_org_default_change_orig as 
+select ad_column_id, defaultvalue
+from ad_column
+where lower(columnname) = 'ad_org_id' 
+and ad_table_id in
+(select ad_table_id 
+	from ad_table where lower(tablename) in 
+		(select tablename from chuboe_favorite_ad_org_default_change)
+)
+;
+
+update ad_column 
+set defaultvalue = '0'
+where lower(columnname) = 'ad_org_id' 
+and ad_table_id in
+(select ad_table_id 
+	from ad_table where lower(tablename) in 
+		(select tablename from chuboe_favorite_ad_org_default_change)
+)
+;
+
+--use the following queries to put the tabs and fields back to their original state
+--update ad_column set defaultvalue = (select defaultvalue from chuboe_favorite_ad_org_default_change_orig x where ad_column.ad_column_id = x.ad_column_id) where ad_column_id in (select ad_column_id from chuboe_favorite_ad_org_default_change_orig);
