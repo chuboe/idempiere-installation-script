e08fa77f0e01056dea16801a2cc0458bde81c8eb
Chuck Boecking <chuck@chuboe.com>
2014-10-15 03:40:38 +0000

    chuboe_favorite_misc_change.sql edited online with Bitbucket

--- favorites/chuboe_favorite_misc_change.sql
+++ favorites/chuboe_favorite_misc_change.sql
@@ -39,3 +39,11 @@ set ad_reference_id = 30
 where AD_Reference_ID in (19, 18) --table direct, table
 and lower(columnname) in ('ad_user_id', 'c_bpartner_id', 'c_bpartner_location_id', 'createdby', 'updatedby', 'c_order_id', 'c_orderline_id', 'c_invoice_id', 'c_invoiceline_id', 'm_inout_id', 'm_inoutline_id', 'bill_bpartner_id', 'bill_location_id', 'bill_user_id', 'salesrep_id')
 ;
+
+--update the accounting schema
+update C_AcctSchema
+set costingmethod = 'S', --standard costing
+costinglevel = 'O', --org level costing
+autoperiodcontrol = 'N', 
+isallownegativeposting = 'N'
+;
