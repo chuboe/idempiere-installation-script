faa108b3ff8457d20fbd861a102e786aaeb6d9f8
Chuck Boecking <chuck@chuboe.com>
2014-11-03 21:18:19 +0000

    idempiere_install_script_master_linux.sh edited online with Bitbucket

--- idempiere_install_script_master_linux.sh
+++ idempiere_install_script_master_linux.sh
@@ -84,7 +84,7 @@ CHUBOE_PROP="$CHUBOE_UTIL/properties"
 INITDNAME="idempiere"
 SCRIPTNAME=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPTNAME")
-JENKINSPROJECT="iDempiere2.0Daily"
+JENKINSPROJECT="iDempiere2.1Daily"
 ECLIPSESOURCEPATH="http://download.springsource.com/release/ECLIPSE/kepler/SR1/eclipse-jee-kepler-SR1-linux-gtk-x86_64.tar.gz"
 OSUSER="ubuntu"
 IDEMPIEREUSER="idempiere"
@@ -146,10 +146,10 @@ do
 	esac
 done
 
-IDEMPIERECLIENTPATH="http://superb-dca2.dl.sourceforge.net/project/idempiere/v2.0/swing-client/idempiereClient.gtk.linux.x86_64.zip"
-IDEMPIERECLIENTPATHBLEED="http://jenkins.idempiere.com/job/$JENKINSPROJECT/ws/buckminster.output/org.adempiere.ui.swing_2.0.0-eclipse.feature/idempiereClient.gtk.linux.x86_64.zip"
-IDEMPIERESOURCEPATH="http://superb-dca2.dl.sourceforge.net/project/idempiere/v2.0/server/idempiereServer.gtk.linux.x86_64.zip"
-IDEMPIERESOURCEPATHBLEED="http://jenkins.idempiere.com/job/$JENKINSPROJECT/ws/buckminster.output/org.adempiere.server_2.0.0-eclipse.feature/idempiereServer.gtk.linux.x86_64.zip"
+IDEMPIERECLIENTPATH="http://superb-dca2.dl.sourceforge.net/project/idempiere/v2.1/swing-client/idempiereClient.gtk.linux.x86_64.zip"
+IDEMPIERECLIENTPATHBLEED="http://jenkins.idempiere.com/job/$JENKINSPROJECT/ws/buckminster.output/org.adempiere.ui.swing_2.1.0-eclipse.feature/idempiereClient.gtk.linux.x86_64.zip"
+IDEMPIERESOURCEPATH="http://superb-dca2.dl.sourceforge.net/project/idempiere/v2.1/server/idempiereServer.gtk.linux.x86_64.zip"
+IDEMPIERESOURCEPATHBLEED="http://jenkins.idempiere.com/job/$JENKINSPROJECT/ws/buckminster.output/org.adempiere.server_2.1.0-eclipse.feature/idempiereServer.gtk.linux.x86_64.zip"
 IDEMPIERESOURCEPATHBLEEDDETAIL="http://jenkins.idempiere.com/job/$JENKINSPROJECT/changes"
 HOME_DIR="/home/$OSUSER"
 
