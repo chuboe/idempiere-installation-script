9f6141b10e753c784647bea5ee50d3ee07928938
Chuck Boecking <chuck@chuboe.com>
2015-03-24 02:54:44 +0000

    Fixed issue relating to pushing multiple latest files

--- utils/chuboe_backup_s3cmd.sh
+++ utils/chuboe_backup_s3cmd.sh
@@ -2,6 +2,7 @@
 # Version 1 Mike Stroven - created
 # Version 2 Sandy Corsillo - enhanced to use s3cmd
 # Version 3 Chuck Boecking - added more variables
+# Version 4 Chuck Boecking - fixed bug where script copied multiple files to latest
 LOGFILE="/log/chuboe_db_backup.log"
 ADEMROOTDIR="/opt/idempiere-server"
 LOCALBACKDIR="chuboe_backup"
@@ -11,37 +12,42 @@ echo LOGFILE="$ADEMROOTDIR"/"$LOGFILE" >> "$ADEMROOTDIR"/"$LOGFILE"
 echo ADEMROOTDIR="$ADEMROOTDIR" >> "$ADEMROOTDIR"/"$LOGFILE"
 
 cd "$ADEMROOTDIR"/utils
-echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo edembak: -------          STARTING iDempiere Daily Backup            ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo adembak: Executing RUN_DBExport.sh local backup utility. >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  -------          STARTING iDempiere Daily Backup            ------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  Executing RUN_DBExport.sh local backup utility. >> "$ADEMROOTDIR"/"$LOGFILE"
 if "$ADEMROOTDIR"/utils/RUN_DBExport.sh >> "$ADEMROOTDIR"/"$LOGFILE"
 mv "$ADEMROOTDIR"/data/ExpDat????????_??????.jar "$ADEMROOTDIR"/"$LOCALBACKDIR"/
 then
-    echo adembak: Local Backup Succeeded.  Copying to S3 bucket. >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo Prepare latest directory >> "$ADEMROOTDIR"/"$LOGFILE"
+    mkdir "$ADEMROOTDIR"/"$LOCALBACKDIR"/latest/
+    rm "$ADEMROOTDIR"/"$LOCALBACKDIR"/latest/*.jar
+    cd "$ADEMROOTDIR"/"$LOCALBACKDIR"/
+
+    #copy most recent backup to latest folder
+    ls -t ExpDat*.jar | head -1 | awk '{print "cp " $0 " latest/"$0}' | sh
+
+    echo Local Backup Succeeded.  Copying to S3 bucket. >> "$ADEMROOTDIR"/"$LOGFILE"
     if s3cmd sync "$ADEMROOTDIR"/"$LOCALBACKDIR"/ s3://"$S3BUCKET"/
-       s3cmd sync --delete "$ADEMROOTDIR"/"$LOCALBACKDIR"/ s3://"$S3BUCKET"/latest/
+       s3cmd sync --delete "$ADEMROOTDIR"/"$LOCALBACKDIR"/latest/ s3://"$S3BUCKET"/latest/
     then
-        echo adembak: Copy of backup file succeeded.  Deleting local copy. >> "$ADEMROOTDIR"/"$LOGFILE"
-        cd "$ADEMROOTDIR"/"$LOCALBACKDIR"
-        rm "$ADEMROOTDIR"/"$LOCALBACKDIR"/ExpDat????????_??????.jar >> "$ADEMROOTDIR"/"$LOGFILE"
-        echo adembak: Local copy deleted. >> "$ADEMROOTDIR"/"$LOGFILE"
+        echo Copy of backup files to S3 succeeded. >> "$ADEMROOTDIR"/"$LOGFILE"
     else
-        echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-        echo adembak: -------         Remote iDempiere Backup FAILED!             ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-        echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-        echo . 
+        echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+        echo  -------         Remote iDempiere Backup FAILED!             ------- >> "$ADEMROOTDIR"/"$LOGFILE"
+        echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+        echo .
         exit 1
     fi
 else
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: -------          Local iDempiere Backup FAILED!             ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo  .
+    echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo  -------          Local iDempiere Backup FAILED!             ------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo .
     exit 1
 fi
-echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo edembak: -------         COMPLETED iDempiere Daily Backup            ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  -------         COMPLETED iDempiere Daily Backup            ------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo  ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
 echo .
 exit 0
