516f2c654981da3834d5e64da740403d5eb024bf
Chuck Boecking <chuck@chuboe.com>
2015-03-27 16:09:52 +0000

    chuboe_delete_transactional_data.sh edited online with Bitbucket

--- utils/chuboe_delete_transactional_data.sh
+++ utils/chuboe_delete_transactional_data.sh
@@ -6,6 +6,8 @@
 # This script should only be executed from a development instance - NOT PRODUCTION.
 ######################################
 
+CHUBOE_UTIL_HG="$CHUBOE_UTIL/idempiere-installation-script/"
+CHUBOE_UTIL_HG_PROP="$CHUBOE_UTIL_HG/utils/properties/"
 LOGFILE="/log/chuboe_db_del_trx.log"
 ADEMROOTDIR="/opt/idempiere-server"
 UTILSDIR="chuboe_utils"
@@ -13,39 +15,39 @@ DATABASE="idempiere"
 USER="adempiere"
 ADDPG="-h localhost -p 5432"
 
-echo LOGFILE="$ADEMROOTDIR"/"$LOGFILE" >> "$ADEMROOTDIR"/"$LOGFILE"
-echo ADEMROOTDIR="$ADEMROOTDIR" >> "$ADEMROOTDIR"/"$LOGFILE"
+echo LOGFILE="$CHUBOE_UTIL_HG"/"$LOGFILE" >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+echo ADEMROOTDIR="$ADEMROOTDIR" >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
 
 cd "$ADEMROOTDIR"/utils
-echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo ademres: -------            STARTING iDempiere Delete Trx!           ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+echo ademres: -------            STARTING iDempiere Delete Trx!           ------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
 
-RESULT=$(ls -l $ADEMROOTDIR/$UTILSDIR/properties/CHUBOE_TEST_ENV_YES.txt | wc -l)
+RESULT=$(ls -l $CHUBOE_UTIL_HG_PROP/CHUBOE_TEST_ENV_YES.txt | wc -l)
 if [ $RESULT -ge 1 ]; then
-    echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo ademres: -------              This is a Dev Envrionment              ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo ademres: -------              This is a Dev Envrionment              ------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
 else
-    echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo ademres: -------            STOPPING Not a Dev Envrionment           ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo ademres: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo ademres: -------            STOPPING Not a Dev Envrionment           ------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo ademres: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
     exit 1
 fi #end if dev environment check
-if "$ADEMROOTDIR"/utils/RUN_DBExport.sh &>> "$ADEMROOTDIR"/"$LOGFILE"
+if "$ADEMROOTDIR"/utils/RUN_DBExport.sh &>> "$CHUBOE_UTIL_HG"/"$LOGFILE"
 then
-    echo adembak: Local Backup Succeeded.  >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo adembak: Local Backup Succeeded.  >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
 else
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: -------          Local iDempiere Backup FAILED!             ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+    echo adembak: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo adembak: -------          Local iDempiere Backup FAILED!             ------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo adembak: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
     echo  .
     exit 1
 fi
 
-psql -d $DATABASE -U $USER $ADDPG -f "$ADEMROOTDIR"/"$UTILSDIR"/chuboe_delete_transactional_data.sql &>> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo edembak: -------         FINISHED iDempiere Delete Trx!              ------- >> "$ADEMROOTDIR"/"$LOGFILE"
-    echo adembak: ------------------------------------------------------------------- >> "$ADEMROOTDIR"/"$LOGFILE"
+psql -d $DATABASE -U $USER $ADDPG -f "$CHUBOE_UTIL_HG"/utils/chuboe_delete_transactional_data.sql &>> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo adembak: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo edembak: -------         FINISHED iDempiere Delete Trx!              ------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
+    echo adembak: ------------------------------------------------------------------- >> "$CHUBOE_UTIL_HG"/"$LOGFILE"
     echo .
 exit 0
