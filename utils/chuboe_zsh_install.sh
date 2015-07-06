d4cf695e4fa4966031a6172d40fb25a07793eb85
Chuck Boecking <chuck@chuboe.com>
2015-07-06 20:28:22 +0000

    chuboe_zsh_install.sh edited online with Bitbucket

--- utils/chuboe_zsh_install.sh
+++ utils/chuboe_zsh_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install -y curl git zsh
-sudo bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc	
 sudo sed -i "s|ZSH_THEME=\"robbyrussell\"|ZSH_THEME=\"ys\"|" ~/.zshrc
-
+echo "modify the /etc/passwd file to set zsh as your username's default."
+echo "==> look for something like this: ubuntu:x:1000:1000:Ubuntu:/home/ubuntu:/bin/bash"
+echo "==> replace the bash with zsh like this: ubuntu:x:1000:1000:Ubuntu:/home/ubuntu:/bin/zsh"
