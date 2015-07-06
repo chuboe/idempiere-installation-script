7e96a98ec509902a72d0529060e1250b413197c5
chuck <chuck@chuboe.com>
2015-07-06 19:36:40 +0000

    added sed

--- utils/chuboe_zsh_install.sh
+++ utils/chuboe_zsh_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install -y curl git zsh
-bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sed -i "s|ZSH_THEME=\"robbyrussell\"|ZSH_THEME=\"ys\"|" ~/.zshrc
 
