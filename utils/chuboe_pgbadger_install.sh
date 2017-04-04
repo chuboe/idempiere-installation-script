3b38ead0c7b3ab55d4ef8acb8e5912b86a0062e8
Chuck Boecking <chuck@chuboe.com>
2017-04-04 14:27:27 +0000

    chuboe_pgbadger_install.sh edited online with Bitbucket

--- utils/chuboe_pgbadger_install.sh
+++ utils/chuboe_pgbadger_install.sh
@@ -3,6 +3,14 @@
 # pgbadger anazlyses postgresql logs - http://dalibo.github.io/pgbadger/
 # Note: the ubuntu version through apt-get is old (v3)
 
+# for pgbadger to work, you need to set the appropriate log settings in postgresql.conf
+# The installation script uses https://www.pgconfig.org/#/tuning to tune your database when you install iDempiere and the database on separate machines.
+# Look at the bottom of your /etc/postgresql/$PGVERSION/main/postgresql.conf file to see if the setting have been updated. 
+# If not, use https://www.pgconfig.org/#/tuning to create pgbadger settings and append to your postgresql.conf file.
+# The following commands will do this for you. Note that you must fill in the parameters manually.
+    # curl 'https://api.pgconfig.org/v1/tuning/get-config?environment_name=OLTP&format=conf&include_pgbadger=true&log_format=csvlog&max_connections=100&pg_version='$PGVERSION'&total_ram='$AVAIL_MEMORY'MB' >> $TEMP_DIR/pg.conf
+    # cat $TEMP_DIR/pg.conf | sudo tee -a /etc/postgresql/$PGVERSION/main/postgresql.conf
+
 CURRENT_VER="7.0"
 
 sudo apt-get update
