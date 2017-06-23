266f34ae90815ba7b8b251b061829b9193258632
Chuck Boecking <chuck@chuboe.com>
2017-06-23 03:27:09 +0000

    chuboe_obfuscation.sh edited online with Bitbucket

--- utils/chuboe_obfuscation.sh
+++ utils/chuboe_obfuscation.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Created Version 1 Chuck Boecking
-
-######################################
-# This file is dangerous. It will overwrite your database.
-# This script should only be executed from a development instance - NOT PRODUCTION.
-######################################
+# Version 2 Chuck Boecking - moved obfuscation to separate database
 
 #bring chuboe.properties into context
 source chuboe.properties
@@ -17,54 +13,66 @@ DATABASE=$CHUBOE_PROP_DB_NAME
 IDEMPIEREUSER=$CHUBOE_PROP_IDEMPIERE_OS_USER
 USER=$CHUBOE_PROP_DB_USERNAME
 ADDPG="-h $CHUBOE_PROP_DB_HOST -p $CHUBOE_PROP_DB_PORT"
+EXPORT_DIR="/tmp/"
 DATABASE_OB="obfus"
-DATABASE_TMP_EXPORT="/tmp/obtempout.bak"
+DATABASE_TMP_EXPORT="obtempout.bak"
+DATABASE_OB_EXPORT="ExpDatObfus.dmp"
+DATABASE_OB_JAR="ExpDatObfus_"`date +%Y%m%d`_`date +%H%M%S`".jar"
 
-echo LOGFILE=$LOGFILE >> $LOGFILE
-echo NOTE: writing logs here: $LOGFILE
-echo ADEMROOTDIR=$ADEMROOTDIR >> $LOGFILE
+echo ADEMROOTDIR=$ADEMROOTDIR
 
 cd $ADEMROOTDIR/utils
-echo ademres: ------------------------------------------------------------------- >> $LOGFILE
-echo ademres: -------            STARTING iDempiere Obfuscation           ------- >> $LOGFILE
-echo ademres: ------------------------------------------------------------------- >> $LOGFILE
+echo ademres: -------------------------------------------------------------------
+echo ademres: -------            STARTING iDempiere Obfuscation           -------
+echo ademres: -------------------------------------------------------------------
 
 if [[ $CHUBOE_PROP_IS_TEST_ENV = "Y" ]]; then
-    echo ademres: ------------------------------------------------------------------- >> $LOGFILE
-    echo ademres: -------              This is a Dev Envrionment              ------- >> $LOGFILE
-    echo ademres: ------------------------------------------------------------------- >> $LOGFILE
+    echo ademres: -------------------------------------------------------------------
+    echo ademres: -------              This is a Dev Envrionment              -------
+    echo ademres: -------------------------------------------------------------------
 else
-    echo ademres: ------------------------------------------------------------------- >> $LOGFILE
-    echo ademres: -------            STOPPING Not a Dev Envrionment           ------- >> $LOGFILE
-    echo ademres: ------------------------------------------------------------------- >> $LOGFILE
+    echo ademres: -------------------------------------------------------------------
+    echo ademres: -------            STOPPING Not a Dev Envrionment           -------
+    echo ademres: -------------------------------------------------------------------
     exit 1
 fi #end if dev environment check
 echo NOTE: Ignore errors related to myEnvironment.sav
-if sudo -u $IDEMPIEREUSER "$ADEMROOTDIR"/utils/RUN_DBExport.sh >> $LOGFILE
+if sudo -u $IDEMPIEREUSER "$ADEMROOTDIR"/utils/RUN_DBExport.sh
 then
-    echo adembak: Local Backup Succeeded.  >> $LOGFILE
+    echo adembak: Local Backup Succeeded. 
 else
-    echo adembak: ------------------------------------------------------------------- >> $LOGFILE
-    echo adembak: -------          Local iDempiere Backup FAILED!             ------- >> $LOGFILE
-    echo adembak: ------------------------------------------------------------------- >> $LOGFILE
+    echo adembak: -------------------------------------------------------------------
+    echo adembak: -------          Local iDempiere Backup FAILED!             -------
+    echo adembak: -------------------------------------------------------------------
     echo  .
     exit 1
 fi
 
-#drop the existing obfuscated database if present
-sudo -u postgres dropdb $DATABASE_OB
-#export the existing database
-sudo -u postgres pg_dump $DATABASE -Fc > $DATABASE_TMP_EXPORT
+#drop the obfuscated database if present
+echo NOTE: ignore errors on drop obfuscated database
+dropdb $ADDPG -U $USER $DATABASE_OB
+#export the existing iDempiere database
+pg_dump $ADDPG -U $USER $DATABASE -Fc > $EXPORT_DIR/$DATABASE_TMP_EXPORT
 #create the obfuscated database
-sudo -u postgres createdb $DATABASE_OB
-#restore existing database to obfuscated database
-sudo -u postgres pg_restore -Fc -d $DATABASE_OB $DATABASE_TMP_EXPORT
+createdb $ADDPG -U $USER $DATABASE_OB
+#restore existing iDempiere database to obfuscated database
+pg_restore $ADDPG -U $USER -Fc -d $DATABASE_OB $EXPORT_DIR/$DATABASE_TMP_EXPORT
 #remove old database export file
-sudo rm $DATABASE_TMP_EXPORT
+sudo rm $EXPORT_DIR/$DATABASE_TMP_EXPORT
 
-psql -d $DATABASE_OB -U $USER $ADDPG -f "$CHUBOE_UTIL_HG"/utils/chuboe_obfuscation.sql >> $LOGFILE
-    echo adembak: ------------------------------------------------------------------- >> $LOGFILE
-    echo edembak: -------         FINISHED iDempiere Obfuscation              ------- >> $LOGFILE
-    echo adembak: ------------------------------------------------------------------- >> $LOGFILE
-    echo .
+#execute the obfuscation sql script
+psql -d $DATABASE_OB -U $USER $ADDPG -f "$CHUBOE_UTIL_HG"/utils/chuboe_obfuscation.sql
+
+#dump the obfuscated database
+pg_dump $ADDPG --no-owner -U $USER $DATABASE_OB > $EXPORT_DIR/$DATABASE_OB_EXPORT
+
+#jar export
+cd $EXPORT_DIR
+jar cvfM $DATABASE_OB_JAR $DATABASE_OB_EXPORT
+echo NOTE: you can find the exported database here: $EXPORT_DIR/$DATABASE_OB_JAR
+
+echo adembak: -------------------------------------------------------------------
+echo edembak: -------         FINISHED iDempiere Obfuscation              -------
+echo adembak: -------------------------------------------------------------------
+echo .
 exit 0
