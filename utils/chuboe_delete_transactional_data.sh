ca7af0f0a35c01e1c7bb6c931b55298b3862988a
Chuck Boecking <chuck@chuboe.com>
2017-09-07 20:22:13 +0000

    chuboe_delete_transactional_data.sh edited online with Bitbucket

--- utils/chuboe_delete_transactional_data.sh
+++ utils/chuboe_delete_transactional_data.sh
@@ -36,7 +36,6 @@ else
     exit 1
 fi #end if dev environment check
 echo running script...
-echo NOTE: ignore errors relating to myEnvironment.sav
 if sudo -u $IDEMPIEREUSER "$ADEMROOTDIR"/utils/RUN_DBExport.sh &>> "$LOGFILE"
 then
     echo adembak: Local Backup Succeeded.  >> "$LOGFILE"
