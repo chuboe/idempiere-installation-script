d3e92ba7ca4c6a1f0a325deb1c09f663b1b7ff0b
Chuck Boecking <chuck@chuboe.com>
2018-05-04 02:00:18 +0000

    validate HOSTPATH before downloading

--- utils/download.sh
+++ utils/download.sh
@@ -5,20 +5,26 @@
 #  2 - Filename
 #  3 - local path prefix including trailing /
 #  4 - (Optional) Jenkins Auth command
+    
+    # preprocess the URL to ensure no double forward slash exists except for ://
+    # remove double slashes = sed s#//*#/#g
+    # add back :// = sed s#:/#://#g
+    HOSTPATH_URL=$(echo $1 | sed s#//*#/#g | sed s#:/#://#g)
+    
     # wget --unlink doesn't remove the file, must use rm
     # Must remove the file because if it exists in a corrupted state wget will not fix it, instead it will make a new file named .1
     if [ -e $3/$2 ]; then { rm $3/$2 ; } fi
     if [ -e $3/$2.md5 ]; then { rm $3/$2.md5 ; } fi
 
-    # Remove the -nv if you want to see detailed downloading progress in output.txt
-    echo "DOWNLOAD FILE: wget $4 $1$2 -P $3 2>&1"
-    wget $4 $1$2 -P $3 2>&1
-    if [ $? -ne 0 ]; then { echo "HERE: Can't download $1$2"; exit 1; } fi
+    # add -nv if you do not want to see detailed downloading progress in output.txt
+    echo "DOWNLOAD FILE: wget $4 $HOSTPATH_URL$2 -P $3 2>&1"
+    wget $4 $HOSTPATH_URL$2 -P $3 2>&1
+    if [ $? -ne 0 ]; then { echo "HERE: Can't download $HOSTPATH_URL$2"; exit 1; } fi
 
     # Check to see if md5 exists. If so, downloaded md5 file
-    echo "DOWNLOAD MD5: wget $4 $1$2.md5 -P $3 2>&1"
-    wget $4 $1$2.md5 -P $3 2>&1
-    if [ $? -ne 0 ]; then { echo "HERE: Can't download $1$2.md5. This is not a fatal error, but cannot verify download."; exit 0; } fi
+    echo "DOWNLOAD MD5: wget $4 $HOSTPATH_URL$2.md5 -P $3 2>&1"
+    wget $4 $HOSTPATH_URL$2.md5 -P $3 2>&1
+    if [ $? -ne 0 ]; then { echo "HERE: Can't download $HOSTPATH_URL$2.md5. This is not a fatal error, but cannot verify download."; exit 0; } fi
 
     # Check download against md5
     # NOTE: use the md5 command to create a test file. Note: use the same file name with a .md5 suffix.
