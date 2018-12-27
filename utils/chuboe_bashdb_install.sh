8f9825213f1a8097872b4d1a882ab82a66d68132
Chuck Boecking <chuck@chuboe.com>
2018-12-27 23:49:31 +0000

    chuboe_bashdb_install.sh edited online with Bitbucket

--- utils/chuboe_bashdb_install.sh
+++ utils/chuboe_bashdb_install.sh
@@ -2,6 +2,11 @@
 
 # install bashdb manually since no longer supported by ubuntu 18.04
 
+# Use the following command to launch bashdb and ensure file paths are preserved
+# 	bash --debugger ./idempiere-installation-script/idempiere_install_script_master_linux.sh -- -P Silly -u ubuntu -l &>output.txt
+# NOTE: the -- before the script arguments - this tells bash to use the arguements for your script - not bash itself
+# http://bashdb.sourceforge.net/bashdb.html#Having-bash-invoke-the-debugger-and-your-shell-script
+
 sudo apt-get -y update
 sudo apt-get -y install texi2html texinfo build-essential
 
