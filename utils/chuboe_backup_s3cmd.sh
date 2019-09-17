927e1c07192fe231d3bbe69282f355b69b7b8f15
Chuck Boecking <chuck@chuboe.com>
2019-09-17 21:53:13 +0000

    chuboe_backup_s3cmd.sh edited online with Bitbucket

--- utils/chuboe_backup_s3cmd.sh
+++ utils/chuboe_backup_s3cmd.sh
@@ -52,7 +52,7 @@ then
     echo Local Backup Succeeded.  Copying to S3 bucket. >> "$LOGFILE"
     if 
         # fully qualified path to support running from cron
-        /usr/local/bin/s3cmd sync --delete-removed "$LOCALBACKDIR"/ s3://"$S3BUCKET"/ >> "$LOGFILE"
+        /usr/local/bin/s3cmd sync "$LOCALBACKDIR"/ s3://"$S3BUCKET"/ >> "$LOGFILE"
         /usr/local/bin/s3cmd sync --delete-removed --delete-after "$LOCALBACKDIR"/latest/ s3://"$S3BUCKET"/latest/ >> "$LOGFILE"
     then
         echo Copy of backup files to S3 succeeded. >> "$LOGFILE"
