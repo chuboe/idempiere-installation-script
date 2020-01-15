320a8ff948e40b4768a33e4b2db73bbfbabd4848
Chuck Boecking <chuck@chuboe.com>
2020-01-15 18:35:13 +0000

    added more  sleep

--- utils/chuboe_osgi_ss.sh
+++ utils/chuboe_osgi_ss.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/expect -f
 #this file will send the current inventory of plugins to console output.
 #example usage: ./chuboe_osgi_ss.sh > somefile.txt
+#sleep to allow for all plugins to deploy
+sleep 60
 spawn telnet localhost 12612
 send -- "ss\n"
 send -- "disconnect\n\n"
