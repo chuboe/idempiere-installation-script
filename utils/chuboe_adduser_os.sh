84e0e534508c67555ae7ac3c39fe25dc1fe62ea4
Chuck Boecking <chuck@chuboe.com>
2020-01-17 12:16:47 +0000

    added ability to run adduser mutiple times

--- utils/chuboe_adduser_os.sh
+++ utils/chuboe_adduser_os.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 USER_TO_ADD=$1
+INSTALL_DATE=`date +%Y%m%d`_`date +%H%M%S`
+PEM_NAME="$USER_TO_ADD"_"$INSTALL_DATE".pem
+PEM_LOCATION=/home/$USER/pem_created
+
+echo Adding user: $USER_TO_ADD
+echo Install date: $INSTALL_DATE
+echo Pem name: $PEM_NAME
+
 sudo adduser $USER_TO_ADD --disabled-password --gecos "$USER_TO_ADD,none,none,none"
 
 # create user credential in the tmp directory
@@ -9,7 +17,7 @@ mkdir -p $TEMP_DIR
 cd $TEMP_DIR
 sudo ssh-keygen -f $USER_TO_ADD -N ''
 # rename pem file
-sudo mv $USER_TO_ADD $USER_TO_ADD.pem
+sudo mv $USER_TO_ADD $PEM_NAME
 
 # make you the current user the owner of these files so you can download .pem later via scp
 sudo chown -R $USER:$USER $TEMP_DIR
@@ -20,5 +28,6 @@ sudo -u $USER_TO_ADD cat $TEMP_DIR/$USER_TO_ADD.pub | sudo tee --append /home/$U
 sudo chmod 600 /home/$USER_TO_ADD/.ssh/authorized_keys
 sudo chown $USER_TO_ADD:$USER_TO_ADD /home/$USER_TO_ADD/.ssh/authorized_keys
 
-mv $TEMP_DIR/$USER_TO_ADD.pem /home/$USER/.
+mkdir -p $PEM_LOCATION
+mv $TEMP_DIR/$PEM_NAME $PEM_LOCATION
 rm -r $TEMP_DIR
