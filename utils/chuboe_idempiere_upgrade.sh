97a0534f4c44b73ca80de8ffde9335dc5b2f2d25
Chuck Boecking <chuck@chuboe.com>
2020-02-04 14:38:32 -0600

    removed -c option because it was not implemented

--- utils/chuboe_idempiere_upgrade.sh
+++ utils/chuboe_idempiere_upgrade.sh
@@ -7,7 +7,7 @@
 # chuboe_idempiere_upgrade.sh
 # 1.0 initial release
 
-# NOTE: be aware that this script does not make a backup of the /opt/idempiere-server directory. 
+# NOTE: be aware that this script does not make a backup of the /opt/idempiere-server directory.
 # If the upgrade goes badly, you will need to have a way to restore your previous directory.
 # Said another way, always perform this upgrade on a test server before executing on a production server.
 
@@ -22,7 +22,6 @@ This script helps you upgrade your iDempiere server
 
 OPTIONS:
 	-h	Help
-	-c	Specify connection options
 	-m	Specify FILE path to migration scripts
 	-M	Specify URL to migration scripts
 	-u	Upgrade URL to p2 directory
@@ -45,7 +44,6 @@ CHUBOE_UTIL=$CHUBOE_PROP_UTIL_PATH
 CHUBOE_UTIL_HG=$CHUBOE_PROP_UTIL_HG_PATH
 IDEMPIEREUSER=$CHUBOE_PROP_IDEMPIERE_OS_USER
 ID_DB_NAME=$CHUBOE_PROP_DB_NAME
-PG_CONNECT="NONE"
 MIGRATION_DIR=$CHUBOE_UTIL_HG"/chuboe_temp/migration"
 JENKINSPROJECT=$CHUBOE_PROP_JENKINS_PROJECT
 JENKINSURL=$CHUBOE_PROP_JENKINS_URL
@@ -62,14 +60,11 @@ INSTALL_DATE=`date +%Y%m%d`_`date +%H%M%S`
 
 # process the specified options
 # the colon after the letter specifies there should be text with the option
-while getopts "hc:m:M:u:rspb" OPTION
+while getopts "hm:M:u:rspb" OPTION
 do
 	case $OPTION in
 		h)	usage
 			exit 1;;
-
-		c)	#Specify connection options
-			PG_CONNECT="-h "$OPTARG;;
 
 		m)	#Specify FILE path to unzipped migration scripts
 			IS_GET_MIGRATION="N"
@@ -98,11 +93,6 @@ done
 
 echo "Do backupDB "$IS_DB_BACKUP
 
-if [[ $PG_CONNECT == "NONE" ]]
-then
-	PG_CONNECT="-h $CHUBOE_PROP_DB_HOST"
-fi
-
 IDEMPIERESOURCEPATHDETAIL="$JENKINSURL/job/$JENKINSPROJECT/ws/${CHUBOE_PROP_JENKINS_BUILD_NUMBER}/changes"
 
 # show variables to the user (debug)
@@ -111,7 +101,6 @@ echo "HERE: print variables"
 echo "SERVER_DIR="$SERVER_DIR
 echo "P2="$P2
 echo "ID_DB_NAME="$ID_DB_NAME
-echo "PG_CONNECT="$PG_CONNECT
 echo "MIGRATION_DIR="$MIGRATION_DIR
 echo "MIGRATION_DOWNLOAD="$MIGRATION_DOWNLOAD
 echo "IS_RESTART_SERVER="$IS_RESTART_SERVER
@@ -146,7 +135,7 @@ then
     # add back :// = sed s#:/#://#g
     MIGRATION_DOWNLOAD=$(echo $MIGRATION_DOWNLOAD | sed 's|//*|/|g' | sed 's|:/|://|g')
     echo "MIGRATION_DOWNLOAD="$MIGRATION_DOWNLOAD
-	
+
     wget $JENKINS_AUTHCOMMAND $MIGRATION_DOWNLOAD
     unzip migration.zip
 fi #end if IS_GET_MIGRATION = Y
@@ -161,7 +150,7 @@ then
     # create a backup of the iDempiere folder before the upgrade
     cd $CHUBOE_UTIL_HG/utils/
     ./chuboe_hg_bindir.sh
-	
+
 	cd $CHUBOE_UTIL
 	sudo rm -r repository*
 
@@ -170,7 +159,7 @@ then
     # add back :// = sed s#:/#://#g
     P2_DOWNLOAD=$(echo $P2 | sed 's|//*|/|g' | sed 's|:/|://|g')
     echo "P2_DOWNLOAD="$P2_DOWNLOAD
-	
+
 	wget $JENKINS_AUTHCOMMAND $P2_DOWNLOAD
 	unzip repository.zip
     # update iDempiere binaries
