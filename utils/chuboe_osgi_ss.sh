ff9c2f8455d9fd0926f1978378618b09be88e9fe
Chuck Boecking <chuck@chuboe.com>
2020-02-16 20:01:26 +0000

    chuboe_osgi_ss.sh edited online with Bitbucket

--- utils/chuboe_osgi_ss.sh
+++ utils/chuboe_osgi_ss.sh
@@ -2,7 +2,7 @@
 #this file will send the current inventory of plugins to console output.
 #example usage: ./chuboe_osgi_ss.sh > somefile.txt
 #sleep to allow for all plugins to deploy
-sleep 60
+sleep 3
 spawn telnet localhost 12612
 send -- "ss\n"
 send -- "disconnect\n\n"
