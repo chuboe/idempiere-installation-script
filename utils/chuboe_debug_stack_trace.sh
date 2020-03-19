1ea6367ec2fced001dc6a3d693a3688b377f4ddf
Chuck Boecking <chuck@chuboe.com>
2020-03-18 19:34:29 -0500

    updated stack trace

--- utils/chuboe_debug_stack_trace.sh
+++ utils/chuboe_debug_stack_trace.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # Adaptation of script from eclipse.org http://wiki.eclipse.org/How_to_report_a_deadlock#jstackSeries_--_jstack_sampling_in_fixed_time_intervals_.28tested_on_Linux.29
+# Adaptation of https://github.com/cqsupport/jstackSeries.sh/blob/master/jstackSeries.sh
 
-pid=`jcmd | grep "[0-9]* /opt/idempiere-server" -o | grep "[0-9]*" -o`
+source chuboe.properties
+
+sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER pid=`jcmd | grep "[0-9]* /opt/idempiere-server" -o | grep "[0-9]*" -o`
 
 count=${1:-10}  # defaults to 10 times
 delay=${2:-1} # defaults to 1 second
 
 while [ $count -gt 0 ]
 do
-    jstack $pid >jstack.$pid.$(date +%s.%N)
-    top -H -b -n1 -p $pid >top.$pid.$(date +%s.%N)
+    jstack $pid >/tmp/jstack.$pid.$(date +%s.%N)
+    top -H -b -n1 -p $pid >/tmp/top.$pid.$(date +%s.%N)
     sleep $delay
     let count--
     echo -n "."
