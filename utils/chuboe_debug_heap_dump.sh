409c4fd1c830511dcc904ab4b0b516ac1221da08
Chuck Boecking <chuck@chuboe.com>
2020-04-13 21:53:10 +0000

    mo better

--- utils/chuboe_debug_heap_dump.sh
+++ utils/chuboe_debug_heap_dump.sh
@@ -2,10 +2,18 @@
 
 source chuboe.properties
 IDPID=`sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jcmd | grep "[0-9]* /opt/idempiere-server" -o | grep "[0-9]*" -o`
+IDDATE=$(date +%s.%N)
 
 echo pid=$IDPID
+echo date=$IDDATE
 
-sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jcmd $IDPID GC.heap_dump /tmp/heap_dump.$IDPID.$(date +%s.%N)
-top -H -b -n1 -p $IDPID |& tee /tmp/top.$IDPID.$(date +%s.%N)
+sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jcmd $IDPID GC.heap_dump /tmp/heap_dump.$IDPID.$IDDATE
+top -H -b -n1 -p $IDPID |& tee /tmp/heap_top.$IDPID.$IDDATE
 
-echo see /tmp/ for output. execute: ls -ltr to see latest files in /tmp/
+echo set file ownership to $USER
+sudo chown $USER:$USER /tmp/*$IDDATE*
+
+echo see /tmp/ for output. execute the following command to see latest files in /tmp/
+echo ls -ltrh /tmp/*$IDDATE*
+echo files created:
+ls -ltrh /tmp/*$IDDATE*
