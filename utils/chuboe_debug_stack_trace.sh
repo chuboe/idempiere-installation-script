69e1f57c478da1b9df51955f6f155a97a80f10a0
Chuck Boecking <chuck@chuboe.com>
2020-04-14 03:12:40 +0000

    chuboe_debug_stack_trace.sh edited online with Bitbucket

--- utils/chuboe_debug_stack_trace.sh
+++ utils/chuboe_debug_stack_trace.sh
@@ -5,6 +5,8 @@
 source chuboe.properties
 pid=`sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jcmd | grep "[0-9]* /opt/idempiere-server" -o | grep "[0-9]*" -o`
 iddate=$(date +%s.%N)
+CHUBOE_AWS_S3_BUCKET_SUB="some-bucket/some-folder"
+CHUBOE_AWS_S3_BUCKET=s3://$CHUBOE_AWS_S3_BUCKET_SUB/
 
 echo pid=$pid
 echo date=$iddate
@@ -14,17 +16,24 @@ delay=${2:-1} # defaults to 1 second
 
 while [ $count -gt 0 ]
 do
-    sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jstack $pid |& tee /tmp/stack_trace.$pid.$iddate.$CHUBOE_PROP_WEBUI_IDENTIFICATION.$count
-    top -H -b -n1 -p $pid |& tee /tmp/stack_top.$pid.$iddate.$CHUBOE_PROP_WEBUI_IDENTIFICATION.$count
+    stack_trace_file=stack_trace.$pid.$iddate.$CHUBOE_PROP_WEBUI_IDENTIFICATION.$count
+    stack_top_file=stack_top.$pid.$iddate.$CHUBOE_PROP_WEBUI_IDENTIFICATION.$count
+
+    sudo -u $CHUBOE_PROP_IDEMPIERE_OS_USER jstack $pid |& tee /tmp/$stack_trace_file
+    top -H -b -n1 -p $pid |& tee /tmp/$stack_top_file
+
+    sudo chown $USER:$USER /tmp/$stack_trace_file
+    echo aws s3 cp /tmp/$stack_trace_file $CHUBOE_AWS_S3_BUCKET
+    aws s3 cp /tmp/$stack_trace_file $CHUBOE_AWS_S3_BUCKET
+
+    sudo chown $USER:$USER /tmp/$stack_top_file
+    echo aws s3 cp /tmp/$stack_top_file $CHUBOE_AWS_S3_BUCKET
+    aws s3 cp /tmp/$stack_top_file $CHUBOE_AWS_S3_BUCKET
+
     sleep $delay
     let count--
     echo -n "."
 done
 
-echo set file ownership to $USER
-sudo chown $USER:$USER /tmp/*$iddate*
-
-echo see /tmp/ for output. execute the following command to see latest files in /tmp/
-echo ls -ltrh /tmp/*$iddate*
 echo files created:
 ls -ltrh /tmp/*$iddate*
