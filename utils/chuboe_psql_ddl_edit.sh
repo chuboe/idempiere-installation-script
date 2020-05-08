5b189a2cd12b43f54b0bd81abd56aef8afc4cc57
Chuck Boecking <chuck@chuboe.com>
2020-05-07 22:15:11 -0500

    updated ddl_edit scripts

---
+++ utils/chuboe_psql_ddl_edit.sh
@@ -0,0 +1,15 @@
+# extract schema only definitions. This is useful when you need to edit objects like functions where many UI do not work as expected.
+
+source chuboe.properties
+
+#check for empty string: -z checks for null
+if [ -z "$1" ]; then
+    echo "No input - you need to pass in the name of a ddl object"
+    exit 1
+fi
+
+export_dir="ddl_export/"
+export_file=$export_dir"ddl_"$1"_export_"`date +%Y%m%d`_`date +%H%M%S`".sql"
+echo See export file: $export_file
+
+pg_dump -U $CHUBOE_PROP_DB_USERNAME -d $CHUBOE_PROP_DB_NAME -h $CHUBOE_PROP_DB_HOST -t $1 --schema-only > $export_file
---
+++ utils/chuboe_psql_ddl_edit_array.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source chuboe.properties
+
+export_dir="ddl_export/"
+export_file=$export_dir"ddl_export_"`date +%Y%m%d`_`date +%H%M%S`".sql"
+echo See export file: $export_file
+
+mkdir -p $export_dir
+
+readarray -t ddl_array < $1
+
+#check for empty string: -z checks for null
+if [ -z "$ddl_array" ]; then
+    echo "No input - you need to pass in a file containing export list"
+    exit 1
+fi
+
+for i in "${ddl_array[@]}"
+do
+    echo --$i
+    #echo pg_dump -U $CHUBOE_PROP_DB_USERNAME -d $CHUBOE_PROP_DB_NAME -h $CHUBOE_PROP_DB_HOST -t $i --schema-only
+    pg_dump -U $CHUBOE_PROP_DB_USERNAME -d $CHUBOE_PROP_DB_NAME -h $CHUBOE_PROP_DB_HOST -t $i --schema-only >> $export_file
+done
