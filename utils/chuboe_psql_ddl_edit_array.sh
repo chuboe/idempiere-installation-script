e15d7dddb1cd59a4703cd8fa5ad2a7be8b81b325
Chuck Boecking <chuck@chuboe.com>
2020-05-07 22:24:06 -0500

    edit order of ddl_edit_array

--- utils/chuboe_psql_ddl_edit_array.sh
+++ utils/chuboe_psql_ddl_edit_array.sh
@@ -2,12 +2,6 @@
 
 source chuboe.properties
 
-export_dir="ddl_export/"
-export_file=$export_dir"ddl_export_"`date +%Y%m%d`_`date +%H%M%S`".sql"
-echo See export file: $export_file
-
-mkdir -p $export_dir
-
 readarray -t ddl_array < $1
 
 #check for empty string: -z checks for null
@@ -15,6 +9,12 @@ if [ -z "$ddl_array" ]; then
     echo "No input - you need to pass in a file containing export list"
     exit 1
 fi
+
+export_dir="ddl_export/"
+export_file=$export_dir"ddl_export_"`date +%Y%m%d`_`date +%H%M%S`".sql"
+echo See export file: $export_file
+
+mkdir -p $export_dir
 
 for i in "${ddl_array[@]}"
 do
