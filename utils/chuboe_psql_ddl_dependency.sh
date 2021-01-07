3e4ff851b06f584a534e76e209960181d71dc7bc
cboecking <chuck@chuboe.com>
2021-01-06 20:35:36 -0600

    Update chuboe_psql_ddl_dependency.sh

--- utils/chuboe_psql_ddl_dependency.sh
+++ utils/chuboe_psql_ddl_dependency.sh
@@ -1,5 +1,5 @@
 #create a file of dependent objects
-#use chuboe_psql_ddl_edit_array.sh to create sql backup file of dependent objects
+#use this file first, then use use chuboe_psql_ddl_edit_array.sh to create sql backup file of dependent objects
 #once you have a backup of sql, you can drop and recreate the object
 #note; you might need to recreate priviledges and refresh materialized views
 
