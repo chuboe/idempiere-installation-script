ed0e4bed1c1321133e8a7ad8bfcb590fa6b601f1
Chuck Boecking <chuck@chuboe.com>
2022-04-28 23:18:28 -0500

    added backup create s3 artifacts

---
+++ utils/chuboe_backup_create_s3_artifacts.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+#references - s3
+#https://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html
+#https://docs.aws.amazon.com/cli/latest/reference/s3api/put-object-lock-configuration.html
+
+#references - iam-policy
+#https://stackoverflow.com/questions/59103954/how-to-create-a-policy-using-the-aws-cli
+
+#variables
+COMPANY_NAME=delme07 #changeme
+APPLICATION_NAME=idempiere
+DEVELOPMENT_TEAM_NAME=logilite #changeme
+COMPLIANCE_DAYS=50 #changeme
+COMPLIANCE_YEARS=0
+REGION="us-east-1"
+
+#variables derived
+BUCKET_NAME_LATEST="$COMPANY_NAME-$APPLICATION_NAME-latest"
+BUCKET_NAME_ARCHIVE="$COMPANY_NAME-$APPLICATION_NAME-archive"
+BUCKET_NAME_OBFUSCATE="$COMPANY_NAME-$APPLICATION_NAME-obfuscate"
+
+POLICY_WRITER_NAME="s3-$COMPANY_NAME-$APPLICATION_NAME-bucket-writer"
+POLICY_DEVELOPER_NAME="s3-$COMPANY_NAME-$APPLICATION_NAME-$DEVELOPMENT_TEAM_NAME-bucket-developer"
+
+IAM_WRITER_NAME=""
+IAM_DEVELOPER_NAME=""
+
+DELETEME=$1
+
+#delete resources and exit if requested - makes for easy testing and cleanup
+if [ "$DELETEME" == "deleteme" ]
+then
+    aws s3api delete-bucket --bucket $BUCKET_NAME_LATEST
+    aws s3api delete-bucket --bucket $BUCKET_NAME_ARCHIVE
+    aws s3api delete-bucket --bucket $BUCKET_NAME_OBFUSCATE
+
+	#aws iam delete-policy --policy-arn "arn:aws:s3:::'$BUCKET_NAME_OBFUSCATE'/*"
+	#aws iam delete-policy --policy-arn "arn:aws:s3:::'$BUCKET_NAME_OBFUSCATE'/*"
+
+    exit 0
+fi
+
+#create policy for writing to buckets
+aws iam create-policy \
+    --policy-name "$POLICY_WRITER_NAME" \
+    --policy-document \
+'{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObjectAcl",
+                "s3:GetObject",
+                "s3:ListBucketMultipartUploads",
+                "s3:GetObjectRetention",
+                "s3:ListBucketVersions",
+                "s3:GetObjectTagging",
+                "s3:ListBucket",
+                "s3:GetObjectLegalHold",
+                "s3:ListMultipartUploadParts",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::'$BUCKET_NAME_LATEST'/*",
+                "arn:aws:s3:::'$BUCKET_NAME_ARCHIVE'/*",
+                "arn:aws:s3:::'$BUCKET_NAME_OBFUSCATE'/*"
+            ]
+        }
+    ]
+}' \
+| tee $POLICY_WRITER_NAME
+
+#create policy for development team reading from buckets
+aws iam create-policy \
+    --policy-name "$POLICY_DEVELOPER_NAME" \
+    --policy-document \
+'{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::'$BUCKET_NAME_OBFUSCATE'/*"
+            ]
+        }
+    ]
+}' \
+| tee $POLICY_DEVELOPER_NAME
+
+exit 0
+
+#create iam for backup writer
+
+
+#create iam for development team reader
+
+
+#create bucket latest
+aws s3api create-bucket --bucket $BUCKET_NAME_LATEST --region $REGION
+aws s3api put-public-access-block \
+    --bucket $BUCKET_NAME_LATEST \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
+
+#create bucket archive
+aws s3api create-bucket --bucket $BUCKET_NAME_ARCHIVE --region $REGION --object-lock-enabled-for-bucket
+aws s3api put-object-lock-configuration \
+	--bucket $BUCKET_NAME_ARCHIVE \
+	--object-lock-configuration '{ "ObjectLockEnabled": "Enabled", "Rule": { "DefaultRetention": { "Mode": "COMPLIANCE", "Days": 50 }}}'
+aws s3api put-public-access-block \
+    --bucket $BUCKET_NAME_ARCHIVE \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
+
+#create bucket obfuscate
+aws s3api create-bucket --bucket $BUCKET_NAME_OBFUSCATE --region $REGION
+aws s3api put-public-access-block \
+    --bucket $BUCKET_NAME_OBFUSCATE \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
