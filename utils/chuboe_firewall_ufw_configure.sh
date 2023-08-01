df9076e1ec5cc07256647041c13815010d78959a
cboecking <chuck@chuboe.com>
2023-08-01 08:55:46 -0500

    Update chuboe_firewall_ufw_configure.sh

--- utils/chuboe_firewall_ufw_configure.sh
+++ utils/chuboe_firewall_ufw_configure.sh
@@ -8,5 +8,11 @@ sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
+sudo ufw allow from some.ip.address.here to any port 5432 comment 'only allow app server to connect directly to the database'
+# above shows how to add a comment to the rule for future reference
+sudo ufw show added
+# above shows rules even when not active
+  # below is an alternative way to see rules when UFW inactive:
+  sudo cat /etc/ufw/user.rules
 sudo ufw enable
 sudo ufw status
