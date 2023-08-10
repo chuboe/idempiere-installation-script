af70f25f92b5b18757d70fcb154db492bf9e5b8b
cboecking <chuck@chuboe.com>
2023-08-10 14:08:30 -0500

    Update chuboe_docker_compose_install.sh

--- utils/chuboe_docker_compose_install.sh
+++ utils/chuboe_docker_compose_install.sh
@@ -4,12 +4,27 @@
 # You can either run this file as a script or simply copy/paste commands as is needed.
 # Docker-compose is available through apt install on some versions of ubuntu (not aws). The below should get you the latest versions.
 
-# Install Docker - https://linuxize.com/post/how-to-install-and-use-docker-on-ubuntu-18-04/
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# If you are installing docker inside LXD, you need to issue the following from your host.
+# the below commands assume your LXD/LXC instance is named docker3
+lxc config set docker3 security.nesting=true
+lxc config set docker3 security.syscalls.intercept.mknod=true
+lxc config set docker3 security.syscalls.intercept.setxattr=true
+
+# Install Docker - https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install -y docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER # add your user to docker group
+exit
+#reconnect to server where you installed docker and run the hello world example container
+docker run hello-world
 
 # Check to see if all executed as expected
 # sudo service docker status
