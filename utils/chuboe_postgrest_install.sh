7e20b32b8205c4ff008740fc2301e14294ee95ab
cboecking <chuck@chuboe.com>
2023-08-29 10:40:36 -0500

    Update chuboe_postgrest_install.sh

--- utils/chuboe_postgrest_install.sh
+++ utils/chuboe_postgrest_install.sh
@@ -44,3 +44,66 @@ echo 'Read the end the file for instructions to launch postgrest'
 
 # How to import json data into excel
 # https://raw-labs.com/blog/retrieving-json-data-from-a-rest-api-in-excel-with-power-query/
+
+# Below is a summary view that you can use to demonstrate api access at a higher level
+# At the bottom, there exists a grant statement that makes the view available to postegrest
+# example call: curl http://localhost:3000/business_partner
+#create or replace view business_partner as
+#with last_payment as (
+#    select
+#    max(p.c_payment_id) as c_payment_id,
+#    p.c_bpartner_id
+#    from c_payment p
+#    where p.docstatus = 'CO'
+#    group by c_bpartner_id
+#),
+#primary_address as (
+#    select
+#    max(bpl.c_bpartner_location_id) as c_bpartner_location_id,
+#    bpl.c_bpartner_id
+#    from c_bpartner_location bpl
+#    where bpl.isactive='Y'
+#    group by bpl.c_bpartner_id
+#),
+#primary_contact as (
+#    select
+#    max(u.ad_user_id) as ad_user_id,
+#    u.c_bpartner_id
+#    from ad_user u
+#    where u.isactive='Y'
+#    group by u.c_bpartner_id
+#)
+#select
+#bp.name,
+#bp.value as search_key,
+#bp.totalopenbalance,
+#bp.iscustomer,
+#bp.isvendor,
+#coalesce(p.payamt,0) as payment_amount_last,
+#p.dateacct as payment_date_last,
+#coalesce(l.address1,'') as address1,
+#coalesce(l.address2,'') as address2,
+#coalesce(l.city,'') as city,
+#coalesce(l.regionname,r.name,'') as state,
+#coalesce(l.postal,'') as zip,
+#coalesce(u.name,'') as contact_name,
+#coalesce(u.email,'') as contact_email,
+#coalesce(u.phone,'') as contact_phone
+#from c_bpartner bp
+#left join last_payment lp on bp.c_bpartner_id = lp.c_bpartner_id
+#left join c_payment p on lp.c_payment_id = p.c_payment_id
+#left join primary_address pa on bp.c_bpartner_id = pa.c_bpartner_id
+#left join c_bpartner_location bpl on pa.c_bpartner_location_id = bpl.c_bpartner_location_id
+#left join c_location l on bpl.c_location_id = l.c_location_id
+#left join c_region r on l.c_region_id = r.c_region_id
+#left join primary_contact pc on bp.c_bpartner_id = pc.c_bpartner_id
+#left join ad_user u on pc.ad_user_id = u.ad_user_id
+#where bp.isactive = 'Y'
+#and lower(bp.name) not like '%store%'
+#and lower(bp.name) not like '%standard%'
+#and lower(bp.name) not like '%color%'
+#and lower(bp.name) not like '%chrome%'
+#and lower(bp.name) not like '%garden%'
+#;
+#grant select on adempiere.business_partner to postrest_web_anon;
+
