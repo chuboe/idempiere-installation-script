8068573e4a82f724d249906dc23865147369c02d
cboecking <chuck@chuboe.com>
2023-11-17 05:58:47 -0600

    Update chuboe_docker_compose_install.sh - dynamically pull ubuntu vs debian

--- utils/chuboe_docker_compose_install.sh
+++ utils/chuboe_docker_compose_install.sh
@@ -15,7 +15,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/"$(. /etc/os-release && echo "$ID")" \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
